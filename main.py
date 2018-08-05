15fa5f9e615ec5675e640fb70663b129d333d59c
U_1F410 <ottwaym9816@gmail.com>
2018-08-05 23:32:43 +0100

    Added random and multiple websites quickly, Need to add more to creating various threads at different times, Spawn rates and also checking the sockets are pickupable...

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-import time,socket, ssl, threading
+import time,socket, ssl, threading, random
 
 def socketCreate(url,port,delay):
-    print("Opening socket to %s" %url)
+    print("Opening socket to %s for %i" %(url,delay))
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((url,port))
     s = ssl.wrap_socket(s, keyfile=None, certfile=None, server_side=False, cert_reqs=ssl.CERT_NONE, ssl_version=ssl.PROTOCOL_SSLv23)
@@ -16,9 +16,10 @@ def socketCreate(url,port,delay):
         break
 
 def threadController(threadcount):
+    websites = ["github.com","facebook.com","twitter.com"]
     try:
         for i in range(threadcount):
-            threading.Thread(target=socketCreate,args= ("github.com",443,10)).start()
+            threading.Thread(target=socketCreate,args= (random.choice(websites),443,random.randrange(5,120))).start()
     except:
         print("Unable to start threads")
 
